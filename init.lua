f1df03e9f90f2b80150f7112e29e100c5c577f64
SarsaparillaSunrise <149747120+SarsaparillaSunrise@users.noreply.github.com>
2025-01-03 20:52:23 +1300

    Added filesystem tree viewer.

--- init.lua
+++ init.lua
@@ -165,6 +165,18 @@ require("lazy").setup({
 
 
     {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+          "nvim-lua/plenary.nvim",
+          "nvim-tree/nvim-web-devicons",
+          "MunifTanjim/nui.nvim",
+          "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        }
+    },
+
+
+    {
         'stevearc/conform.nvim',
         opts = {},
     },
