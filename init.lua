406a1990b11db3ca31879360e1ef5aa1a6f04f4d
SarsaparillaSunrise <149747120+SarsaparillaSunrise@users.noreply.github.com>
2025-01-03 19:22:33 +1300

    Working lua config.

---
+++ init.lua
@@ -0,0 +1,113 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Basic settings
+vim.o.number = true -- Enable line numbers
+vim.o.tabstop = 4 -- Number of spaces a tab represents
+vim.o.shiftwidth = 4 -- Number of spaces for each indentation
+vim.o.expandtab = true -- Convert tabs to spaces
+vim.o.smartindent = true -- Automatically indent new lines
+vim.o.wrap = false -- Disable line wrapping
+vim.o.cursorline = true -- Highlight the current line
+vim.o.termguicolors = true -- Enable 24-bit RGB colors
+
+-- Syntax highlighting and filetype plugins
+vim.cmd('syntax enable')
+vim.cmd('filetype plugin indent on')
+
+-- Leader key
+vim.g.mapleader = ' ' -- Space as the leader key
+vim.api.nvim_set_keymap('n', '<Leader>w', ':w<CR>', { noremap = true, silent = true })
+
+-- Setup lazy.nvim
+require("lazy").setup({
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = function()
+            require("nvim-autopairs").setup({})
+        end,
+    },
+
+    {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "L3MON4D3/LuaSnip",
+        "rafamadriz/friendly-snippets",
+        "onsails/lspkind.nvim"
+    },
+    event = "InsertEnter",
+    config = function()
+        local cmp = require("cmp")
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body)  -- For `luasnip` users.
+                end,
+            },
+            mapping = {
+                ["<C-n>"] = cmp.mapping.select_next_item(),
+                ["<C-p>"] = cmp.mapping.select_prev_item(),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }),
+            },
+            sources = {
+                { name = "nvim_lsp" },
+                { name = "buffer" },
+                { name = "path" },
+            },
+        })
+    end,
+},
+
+{
+    "neovim/nvim-lspconfig",
+    config = function()
+        require("lspconfig").ts_ls.setup{}  -- Example for TypeScript server
+    end,
+},
+
+{
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+        ensure_installed = { "bash", "html", "javascript", "json", "lua", "python" },  -- Add any other languages you need
+        highlight = { enable = true },
+    }
+},
+
+{
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = { "Telescope" },
+    opts = {
+        defaults = {
+            prompt_prefix = "> ",
+            sorting_strategy = "ascending",
+            layout_strategy = "flex",
+        },
+    }
+},
+
+{
+  'stevearc/conform.nvim',
+  opts = {},
+},
+})
+
