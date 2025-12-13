59babd0db17c8093fd92ffaecdd252f56b0b1e3a
SarsaparillaSunrise <149747120+SarsaparillaSunrise@users.noreply.github.com>
2025-12-13 14:22:13 +1300

    Initial commit.

---
+++ init.lua
@@ -0,0 +1,276 @@
+--[[ 
+  MODERNIZED SINGLE-FILE NEOVIM CONFIG
+  Language: Lua
+  Manager:  Lazy.nvim
+--]]
+
+-- ========================================================================== --
+-- ==                           BOOTSTRAP LAZY                             == --
+-- ========================================================================== --
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- ========================================================================== --
+-- ==                             OPTIONS                                  == --
+-- ========================================================================== --
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+
+-- Appearance
+vim.opt.number = true
+vim.opt.linebreak = true
+vim.opt.termguicolors = true -- Required for modern themes
+
+-- Behavior
+vim.opt.clipboard = "unnamedplus" -- System clipboard
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.splitright = true         -- 'set spr'
+vim.opt.swapfile = false
+vim.opt.hidden = true             -- Buffer switching without saving
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+-- Restore cursor position
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- ========================================================================== --
+-- ==                             KEYMAPS                                  == --
+-- ========================================================================== --
+local keymap = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- Fast saving
+keymap("n", "<leader>s", ":w<CR>", opts)
+keymap("n", "<CR>", ":w<CR>", opts)
+
+-- Standard save (Triggers formatting)
+keymap("n", "<leader>s", ":w<CR>", opts)
+
+-- NEW: Save WITHOUT formatting (Capital S)
+-- :noa = no autocmd (skips the formatting hook)
+keymap("n", "<leader>S", ":noa w<CR>", opts)
+
+-- Buffer management
+keymap("n", "<leader>bd", ":bd<CR>", opts)
+keymap("n", "<Right>", ":bn<CR>", opts)
+keymap("n", "<Left>", ":bp<CR>", opts)
+
+-- End of line
+keymap("n", "`", "$", opts)
+
+-- Emacs bindings for command line
+keymap("c", "<C-A>", "<Home>", { noremap = true })
+keymap("c", "<C-E>", "<End>", { noremap = true })
+keymap("c", "<C-K>", "<C-U>", { noremap = true }) -- Clear line
+
+-- Disable F1
+keymap("n", "<F1>", ":echo<CR>", opts)
+
+-- ========================================================================== --
+-- ==                             PLUGINS                                  == --
+-- ========================================================================== --
+require("lazy").setup({
+
+  -- 1. THEME: Solarized
+  { 
+    "maxmx03/solarized.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.o.background = "dark" -- Default, but we try to detect
+      vim.cmd.colorscheme 'solarized'
+      
+      -- Attempt to respect terminal background changes
+      vim.api.nvim_create_autocmd("OptionSet", {
+        pattern = "background",
+        callback = function()
+          vim.cmd("colorscheme solarized")
+        end,
+      })
+    end
+  },
+
+  -- 2. FILE EXPLORER (Replaces NERDTree)
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    keys = {
+      { "\\", ":Neotree toggle<CR>", desc = "Toggle Explorer" },
+    },
+    opts = {
+      filesystem = {
+        filtered_items = { hide_dotfiles = false, hide_gitignored = false },
+      },
+    }
+  },
+
+  -- 3. TELESCOPE (Replaces FZF)
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>t', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>f', builtin.live_grep, {}) -- "Rg" equivalent
+    end
+  },
+
+  -- 4. TREESITTER (Better Syntax Highlighting)
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "python", "go", "tsx", "typescript", "javascript", "lua", "vim", "vimdoc" },
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end
+  },
+
+  -- 5. GIT SIGNS (The "Git Lens" equivalent)
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require('gitsigns').setup {
+        current_line_blame = true, -- Toggle this to see who wrote the line
+        current_line_blame_opts = {
+          delay = 500,
+        },
+      }
+    end
+  },
+
+  -- 6. STATUS LINE (Replaces Airline)
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = { theme = "solarized_dark" } -- adapts to light/dark automatically often
+  },
+
+  -- 7. AUTO-PAIRS (Replaces your auto-close TODO)
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {} 
+  },
+
+-- 8. FORMATTING
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    opts = {
+      formatters_by_ft = {
+        python = { "ruff_organize_imports", "ruff_format" }, 
+        
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        go = { "gofmt" },
+      },
+      -- This enables Format on Save
+      format_on_save = { timeout_ms = 500, lsp_fallback = true },
+    },
+  },
+
+  -- 9. LSP & AUTOCOMPLETION (Replaces CoC)
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/nvim-cmp",
+      "L3MON4D3/LuaSnip",
+    },
+    config = function()
+      local cmp = require('cmp')
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      
+      -- Setup Mason (Auto-installer)
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "pyright", "ruff", "gopls", "ts_ls" },
+        handlers = {
+          function(server_name)
+            require("lspconfig")[server_name].setup {
+              capabilities = capabilities
+            }
+          end,
+          -- Special handling for Ruff (only for linting/formatting, disable its hover in favor of Pyright)
+          ["ruff"] = function()
+             require("lspconfig").ruff.setup {
+               on_attach = function(client, _)
+                 client.server_capabilities.hoverProvider = false
+               end
+             }
+          end
+        }
+      })
+
+      -- Autocompletion Setup
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item
+          ['<Tab>'] = cmp.mapping.select_next_item(),
+          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' }, -- LSP completion (requests.get...)
+          { name = 'luasnip' },
+        }, {
+          { name = 'buffer' },
+        })
+      })
+      
+      -- Keymaps for LSP (only when LSP is active)
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(ev)
+          local opts = { buffer = ev.buf }
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+          vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+        end,
+      })
+    end
+  }
+})
