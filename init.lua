87561bd38c12ba7642c1d6d14946e98b566719d4
SarsaparillaSunrise <149747120+SarsaparillaSunrise@users.noreply.github.com>
2026-05-22 09:33:03 +1200

    Removed version pin.

--- init.lua
+++ init.lua
@@ -119,7 +119,6 @@ require("lazy").setup({
   -- 3. TELESCOPE (File Finding)
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       local builtin = require('telescope.builtin')
